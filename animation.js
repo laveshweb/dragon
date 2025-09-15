4e0533cc38517c53d2ecb0a4aa64668d319d649c
laveh12 <laveshthecoder@gmail.com>
2025-09-14 20:44:06 -0700

    Your commit message here

--- animation.js
+++ animation.js
@@ -1,8 +1,15 @@
 gsap.from(".bottommain" , {
   scale : 0,
-  durartion : 1,
+  durartion : 2,
 })
 gsap.from(".topmain" , {
   scale : 0,
-  durartion : 1,
+  durartion : 2,
+})
+gsap.from(".full-rounded i" , {
+  scale : 0,
+  duration: 0.8, // fixed spelling
+Color: "#ef3c18e0",
+  repeat : -1,
+  yoyo : true,
 })
