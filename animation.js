57ae364c0fc92e55d4879676b77bbd8fdc073238
laveh12 <laveshthecoder@gmail.com>
2025-09-16 08:29:53 -0700

    Updated home page design and fixed image issue

--- animation.js
+++ animation.js
@@ -13,3 +13,16 @@ Color: "#ef3c18e0",
   repeat : -1,
   yoyo : true,
 })
+
+const items = document.querySelectorAll(".food-item");
+let index = 0;
+
+function showNextItem() {
+  if (window.innerWidth >= 1024) return; // No animation on desktop
+
+  items[index].classList.remove("active");
+  index = (index + 1) % items.length;
+  items[index].classList.add("active");
+}
+
+setInterval(showNextItem, 3000); // change every 3 sec
